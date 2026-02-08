c8d1e1fbef5145d955fd308c3098f44a1426f147
Sujana2004 <sujanakorupolu319@gmail.com>
2026-02-08 17:06:42 +0530

    commit

--- movie-api/src/main/java/com/example/demo/MovieApiApplication.java
+++ movie-api/src/main/java/com/example/demo/MovieApiApplication.java
@@ -8,10 +8,6 @@ public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
-		
-		System.out.println("===============================");
-		System.out.println("URL: http://localhost:5000/api/movies");
-		System.out.println("===============================");
 	}
 
 }
